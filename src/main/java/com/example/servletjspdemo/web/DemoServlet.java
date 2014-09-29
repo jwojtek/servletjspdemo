efb6b2dad63043b9882f77d2411c43d18688a4b7
jwojtek <jwojtek@wojtek-dell.wp-sa.pl>
2014-09-29 12:21:05 +0200

    Licznik

--- src/main/java/com/example/servletjspdemo/web/DemoServlet.java
+++ src/main/java/com/example/servletjspdemo/web/DemoServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("/Demo")
 public class DemoServlet extends HttpServlet {
+	public static int licznik = 0;
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request,
@@ -23,17 +24,30 @@ public class DemoServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		HttpSession session = request.getSession();
 		ServletContext ctx = request.getServletContext();
-		int licznik=0;
+		
+		
+		  {
+		        response.setContentType("text/html");
+		        PrintWriter wynik = response.getWriter();
+
+		        
+		        licznik ++;
+		       wynik.println("Wejsc na strone "+licznik+"<br>");
+		     
+		    }
+		
+	
+		
 		
 		if(param!=null&&param!="")		
 		{
 			    ctx.setAttribute("name", param);
 				session.setAttribute("name", param);
-				licznik++;
+				
 		
 				
 		}
-				out.println("<h1>Wejsc na strone "+licznik+" </h1>");
+				
 				out.println("<h1>Hello from context "+ctx.getAttribute("name")+" </h1>");
 				out.println("<h1>Hello from session "+session.getAttribute("name")+" </h1>");
 				out.println("<h1>Hello from request "+param+" </h1>");
